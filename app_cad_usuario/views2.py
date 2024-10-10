c835a1b7edc1a7ac5ee2662daeb6f0a35da4ad36
Luiz Prudente <luiz.prudente147@gmail.com>
2024-10-10 14:54:10 -0300

    Adicionando feature de cadastro de email e alterando metodo de login

--- app_cad_usuario/views2.py
+++ app_cad_usuario/views2.py
@@ -2,33 +2,55 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
+def valida_senha(senha1,senha2):
+    if senha1 == senha2:
+        return True
+
+def valida_email(email1,email2):
+    if email1 == email2:
+        return True
+
+def valida_senha(senha1,senha2):
+    if senha1 == senha2:
+        return True
+
 def cadastro(request):
     if request.method == "GET":
         return render(request, "usuario/cadastro.html")
     else:
         usuario = request.POST.get('usuario')
         senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        senha_confirm = request.POST.get('senhaconfirm')
+        email_confirm = request.POST.get('emailconfirm')
+        
         
         user = User.objects.filter(username=usuario).first()
         if user:
-            return render(request, "usuario/cadastro.html", {'error_message': 'Usuário já cadastrado'})
+            return render(request, "usuario/cadastro.html", {'error_message': 'Nome de usúario em uso'})
+        
+        check = User.objects.filter(email=email).first()
+        if check:
+            return render(request, "usuario/cadastro.html", {'error_email': 'Email já cadastrado'})
             
+        if valida_senha(senha,senha_confirm) and valida_senha(email,email_confirm):
+            user = User.objects.create_user(username=usuario, password=senha)
+            user.save()
+            return render(request, "usuario/login.html", {'sucessful_message': 'Usuário cadastrado com sucesso'} )
+        else:
+            return render(request, "usuario/cadastro.html", {'error_email_senha': 'Os emails ou senha não conferem'})
         
-        user = User.objects.create_user(username=usuario, password=senha)
-        user.save()
         
-        
-        return render(request, "usuario/login.html", {'sucessful_message': 'Usuário cadastrado com sucesso'} )
         
             
 def user_login(request):
     if request.method == "GET":
         return render(request, 'usuario/login.html')
     else:
-        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
         senha = request.POST.get('senha')
 
-        user = authenticate(username=usuario, password=senha)
+        user = authenticate(email=email, password=senha)
 
         if user:
             login(request, user)
