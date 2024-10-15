6b80c571e45692f0eaa35b4cee46b611e2cf6c2e
Luiz Prudente <luiz.prudente147@gmail.com>
2024-10-14 23:43:23 -0300

    Adicionando lógica para facilitar desenvolvimento quando o banco de dados remoto estiver desativado, garantindo conexão com o banco local

--- setup/settings.py
+++ setup/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv # type: ignore
+import socket
 import os
 
 load_dotenv()
@@ -74,16 +75,42 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": "3306",
-    }
+REMOTE_DB = {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": os.getenv("DB_NAME"),
+    "USER": os.getenv("DB_USER"),
+    "PASSWORD": os.getenv("DB_PASSWORD"),
+    "HOST": os.getenv("DB_HOST"),
+    "PORT": "3306",
 }
+
+
+LOCAL_DB = {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": os.getenv("DB_NAME_REMOTO"),
+    "USER": os.getenv("DB_USER_REMOTO"),
+    "PASSWORD": os.getenv("DB_PASSWORD_REMOTO"),
+    "HOST": os.getenv("DB_HOST_REMOTO"),
+    "PORT": "3306",
+}
+
+def check_conexao_remota():
+    try:
+        socket.create_connection((REMOTE_DB['HOST'], int(REMOTE_DB["PORT"])), timeout=5)
+        return True
+    except (socket.timeout, OSError):
+        return False
+    
+if check_conexao_remota():
+    DATABASES = {
+        'default': REMOTE_DB
+    }
+else:
+    DATABASES = {
+        'default': LOCAL_DB
+    }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
